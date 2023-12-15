d3004bc6023e43bc84b9ae50ab2c2536d3d65225
BrooklinJazz <brooklinjazzmyers@gmail.com>
2023-12-15 01:47:15 -0500

    fix missing verbs and get quiz to run

--- tutor/lib/tutor.ex
+++ tutor/lib/tutor.ex
@@ -3,31 +3,41 @@ defmodule Tutor do
   Documentation for `Tutor`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tutor.hello()
-      :world
-
-  """
-  @tenses File.read!("verbs.json") |> Jason.decode!()
+  @verbs File.read!("verbs.json") |> Jason.decode!()
+  @verb_conjugations File.ls!("./verbs") |> Enum.map(fn file -> "./verbs/#{file}" |> File.read!() |> Jason.decode!() end)
+  # @tenses File.read!("verbs.json") |> Jason.decode!()
 
   def quiz do
-    {tense, conjugations} = Enum.random(@tenses)
+    conjugations = Enum.random(@verb_conjugations)
+    {tense, conjugation} = Enum.random(conjugations)
 
     {french, english} = Enum.random(conjugations)
 
     prompt =
       Enum.random([
-        fn -> translate_to_english(english, french, tense) end,
-        fn -> translate_to_french(english, french, tense) end,
-        # fn -> what_tense_is_this(english, french, tense) end
+        # fn -> translate_to_english(english, french, tense) end,
+        # fn -> translate_to_french(english, french, tense) end,
+        # fn -> what_tense_is_this(english, french, tense) end,
+        fn -> what_french_verb_is_this() end,
+        fn -> what_english_verb_is_this() end
       ])
 
     prompt.()
     quiz()
+  end
+
+  def what_french_verb_is_this do
+    {french, english} = @verbs |> Enum.random()
+
+    answer = IO.gets("what is #{french} in english?\n") |> String.trim()
+    IO.puts("#{english}\n")
+  end
+
+  def what_english_verb_is_this do
+    {french, english} = @verbs |> Enum.random()
+
+    answer = IO.gets("what is #{english} in french?\n") |> String.trim()
+    IO.puts("#{french}\n")
   end
 
   def translate_to_english(english, french, tense) do
