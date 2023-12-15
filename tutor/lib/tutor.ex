8a7af7f5515f47b8146cec42739eb69c1fec6db4
BrooklinJazz <brooklinjazzmyers@gmail.com>
2023-12-15 13:38:36 -0500

    add conjugations exercise

--- tutor/lib/tutor.ex
+++ tutor/lib/tutor.ex
@@ -4,8 +4,10 @@ defmodule Tutor do
   """
 
   @verbs File.read!("verbs.json") |> Jason.decode!()
-  @verb_conjugations File.ls!("./verbs") |> Enum.map(fn file -> "./verbs/#{file}" |> File.read!() |> Jason.decode!() end)
-  # @tenses File.read!("verbs.json") |> Jason.decode!()
+  @verb_conjugations File.ls!("./verbs")
+                     |> Enum.map(fn file ->
+                       "./verbs/#{file}" |> File.read!() |> Jason.decode!()
+                     end)
 
   def quiz do
     conjugations = Enum.random(@verb_conjugations)
@@ -18,8 +20,9 @@ defmodule Tutor do
         # fn -> translate_to_english(english, french, tense) end,
         # fn -> translate_to_french(english, french, tense) end,
         # fn -> what_tense_is_this(english, french, tense) end,
-        fn -> what_french_verb_is_this() end,
-        fn -> what_english_verb_is_this() end
+        # fn -> what_french_verb_is_this() end,
+        # fn -> what_english_verb_is_this() end,
+        fn -> write_conjugations() end
       ])
 
     prompt.()
@@ -77,5 +80,48 @@ defmodule Tutor do
       _ ->
         IO.puts("It is #{tense}")
     end
+  end
+
+  def write_conjugations do
+    {verb, english} = @verbs |> Enum.random()
+    path =
+      "verbs/#{verb_file_name(verb)}.json"
+      |> File.read!()
+      |> Jason.decode!()
+      |> Enum.each(fn {tense, conjugations} ->
+        IO.puts("""
+        =================================================================
+        Write the conjugations for #{verb} (#{english}) in #{tense}
+        =================================================================
+        """)
+        conjugations
+        |> Enum.each(fn {french, english} ->
+          [subject | _tail] = String.split(french, " ")
+
+          IO.gets("#{subject}: ")
+          IO.puts(french)
+        end)
+      end)
+
+    # {tense, conjugations} = @verb_conjugations
+    # # verb
+    # |> Enum.random()
+    # # conjugation
+    # |> Enum.random()
+    # |> Enum.each(fn {french, english} ->
+    #   [subject | _tail] = String.split(french, " ")
+
+    #   IO.gets("write conjugations\n")
+    # end)
+    # |> IO.inspect()
+    Process.sleep(10000)
+  end
+
+  # se réveiller -> se_reveiller
+  defp verb_file_name(verb) do
+    verb
+    |> String.normalize(:nfd)
+    |> String.replace(~r/[^A-z\s]/u, "")
+    |> String.replace(~r/\s/, "_")
   end
 end
